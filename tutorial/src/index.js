1122ea42a702ed41a0023e38ee71c99683f4c775
Chin <mcaiox@icloud.com>
2020-12-07 21:42:30 +0000

    First component in detail + JSX rules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,8 +2,28 @@ import React from "react"; // ES6 Modules imported from node modules
 import ReactDom from "react-dom";
 
 //Capitalised function names tells React this is not a regular function but rather a function component
+
+//Stateless functional component -> must return JSX
 function Greeting() {
-  return <h4>Greetings! This is my first component</h4>;
+  return (
+    //can only a single element -> wrap everything in a div
+    //user camelCase for html attributes
+    //dont use divs, use appropiate names and dont place everything as a div -> follow html semantics
+    <div>
+      <h4>Hello World</h4>
+      <ul>
+        <li>
+          <a href="#">Hello world</a>
+          <button>Button</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+//what happens under the hood -> mshould return html doc syntax -> makes it easier/ readable.
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// };
 
 ReactDom.render(<Greeting />, document.getElementById("root")); //Render(what, where to)  //Render Greeting component and to root in Index.html which has  <div id="root"></div> in <body>
